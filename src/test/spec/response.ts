65f1108952b972b7e41284b1add8d000cb8ccb8f
Joe Cridge <joe.cridge@me.com>
2018-06-26 17:06:46 +0100

    Add extra tests for backwards compatibility

--- src/test/spec/response.ts
+++ src/test/spec/response.ts
@@ -19,15 +19,6 @@ describe('response.ts', () => {
     expect(Response.transformTags('Hey <*>')).to.equal('^Hey ([\\s\\S]*?)$');
     expect(Response.transformTags('Hey <WORD> there')).to.equal('^Hey ([^ ]+?) there$');
   });
-  it('should instantiate <CARDS> correctly', () => {
-    expect(new Response(' <CARDS> ').responseType).to.equal(Attachment.Card);
-  });
-  it('should instantiate <IMAGE> correctly', () => {
-    expect(new Response(' <IMAGE> ').responseType).to.equal(Attachment.Image);
-  });
-  it('should match <CARDS> correctly', () => {
-    expect(new Response(' <CARDS> ').responseType).to.equal(Attachment.Card);
-  });
   it('should match <IMAGE> correctly', () => {
     expect(new Response(' <IMAGE> ').matches({
       user: null,
@@ -39,6 +30,24 @@ describe('response.ts', () => {
     expect(new Response(' <CARDS> ').matches({
       user: null,
       text: null,
+      attachments: [Attachment.Cards],
+    })).to.be.true;
+  });
+  it('should ignore text with <IMAGE> if no text is expected', () => {
+    // This is required for backwards compatibility with when <IMAGE>
+    // could only be used as the entire response.
+    expect(new Response(' <IMAGE> ').matches({
+      user: null,
+      text: ' Here is your image: ',
+      attachments: [Attachment.Image],
+    })).to.be.true;
+  });
+  it('should ignore text with <CARDS> if no text is expected', () => {
+    // This is required for backwards compatibility with when <CARD>
+    // could only be used as the entire response.
+    expect(new Response(' <CARDS> ').matches({
+      user: null,
+      text: ' Here are your options: ',
       attachments: [Attachment.Cards],
     })).to.be.true;
   });
